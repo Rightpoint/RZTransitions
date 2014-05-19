3513ee31ae7955b34b8125ed4ad78fe33f23c1d4
Alex Rouse <alex.rouse@raizlabs.com>
2014-05-19 15:03:26 -0400

    small commit

--- RZTransitions/Transitions/RZAnimationControllerProtocol.h
+++ RZTransitions/Transitions/RZAnimationControllerProtocol.h
@@ -25,7 +25,7 @@
 //  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 //  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-
+ 
 #ifndef RZTransitions_RZAnimationControllerProtocol_h
 #define RZTransitions_RZAnimationControllerProtocol_h
 
